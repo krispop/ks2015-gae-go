fc400099569bca85708e92daef49a915408e0f72
Kris Popendorf <krispop@google.com>
2015-11-27 23:55:33 +0900

    works

--- guests.go
+++ guests.go
@@ -3,14 +3,18 @@ package guests
 import (
 	"fmt"
 	"html/template"
-	"log"
+	"math/rand"
 	"net/http"
 	"time"
+
+	"appengine"
+	"appengine/datastore"
 )
 
 func init() {
 	http.HandleFunc("/", root)
 	http.HandleFunc("/sign", sign)
+	time.Local, _ = time.LoadLocation("Asia/Tokyo")
 }
 
 var rootTemplate = template.Must(template.New("root").Parse(`
@@ -29,32 +33,60 @@ var rootTemplate = template.Must(template.New("root").Parse(`
 </td>
 <td width=50% valign=top>
 {{if .Content}}
-他の人からのメッセージ:<p style="box-shadow: 0 2px 4px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)" align=center>
+他の人からのメッセージ:<p style='border-style: solid; border-color: blue; border-width: 2px;' align=center>
 {{.Content}}
 </p>
+@{{.Time}}
 {{end}}
 </td>
 </body>
 `))
 
 type Message struct {
-	Time    time.Time
+	// Timeは送信されたタイムスタンプ
+	Time time.Time
+	// Contentはメッセージの中身
 	Content string
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	msg := Message{
-		Time:    time.Now(),
-		Content: "test",
+	var msgs []*Message
+	if _, err := datastore.NewQuery("message").GetAll(c, &msgs); err != nil {
+		fmt.Fprint(w, "DB Error :(")
+		c.Errorf("db error: %v", err)
+		return
+
+	}
+	msg := &Message{}
+	c.Infof("messages: %d", len(msgs))
+	if len(msgs) > 0 {
+		msg = msgs[rand.Int31n(int32(len(msgs)))]
 	}
 	if err := rootTemplate.Execute(w, msg); err != nil {
 		fmt.Fprint(w, "Error :(")
-		log.Printf("templating error: %v", err)
+		c.Errorf("templating error: %v", err)
+		return
 	}
 }
 
 func sign(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	msg := r.FormValue("message")
+	if msg == "" {
+		fmt.Fprint(w, "もう少し面白いのを入力してみよう！")
+		return
+	}
+
+	if _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "message", nil), &Message{
+		Time:    time.Now(),
+		Content: msg,
+	}); err != nil {
+		fmt.Fprint(w, "失敗しました。。。 :(")
+		c.Errorf("datastore error: %v", err)
+		return
+	}
 	fmt.Fprint(w, "ありがとうございます！<a href='/'>戻ります</a>")
 }
